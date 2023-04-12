889dd42f53689acef3778e9bb6122445c6cf2029
gitmik7502 <108491891+gitmik7502@users.noreply.github.com>
2023-04-12 16:01:50 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 def prog_tiles():
     import importlib
-    prog2 = importlib.import_module('temp.programshop.test channel')
+    prog1 = importlib.import_module('temp.programshop.test channel')
     from temp.programshop.Tetris import title as prog2title
-    prog_titles = [prog1title(), prog2.title()]
+    prog_titles = [prog1.itle(), prog2title()]
     return prog_titles
