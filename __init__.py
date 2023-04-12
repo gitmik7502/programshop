c519e076ce77a805fcc6ce0d39d22b2f2d62da7c
gitmik7502 <108491891+gitmik7502@users.noreply.github.com>
2023-04-12 15:44:53 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 def prog_tiles():
     from temp.programshop.prog1 import title as prog1title
-    prog_titles = [prog1title()]
+    from temp.programshop.prog2 import title as prog2title
+    prog_titles = [prog1title(), prog2title()]
     return prog_titles
